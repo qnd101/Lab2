4b87f4a370aea426e07cb56b19a5814f1d3be3fb
leeyw <leeywqnd@gmail.com>
2025-10-08 05:10:51 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,12 +7,18 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-cout << "Hello" << endl;
-
-
-
-
-
+   vec = vector<int>();
+   for(int i=1; i<=M; i++) vec.push_back(i);
+   while (true) {
+      for(int j=0;j<M;j++) cout << vec[j];
+      cout << " ";
+      int i=M-1;
+      while(i>=0 && vec[i]==N-M+i+1) i--; 
+      if (i==-1) break;
+      vec[i]++;
+      for (int j=i+1;j<M;j++) vec[j]=vec[i]+(j-i);
+   }
+   cout << endl;
 }
 
 int main() {
